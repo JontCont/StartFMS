364d1842397ecfb0b83fe1e4d3ab7024596aec7c
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-10 12:55:49 +0800

    加入 StartFMS.Extensions.Data library

---
+++ library/StartFMS.Extensions.Data/Models.cs
@@ -0,0 +1,72 @@
+﻿namespace StartFMS.Extensions.Data;
+public static class Models {
+    public static T ToDefaultValue<T>(this T obj) {
+        foreach (var property in obj.GetType().GetProperties()) {
+            if (obj.GetType().GetProperty(property.Name).GetValue(obj) != null) continue;
+            switch (property.PropertyType.Name.ToLower()) {
+                case "string":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "".ToString());
+                    break;
+                case "int":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "0".ToInt());
+                    break;
+                case "int32":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "0".ToInt32());
+                    break;
+                case "int64":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "0".ToInt64());
+                    break;
+                case "double":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "0".ToDecimal());
+                    break;
+                case "decimal":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, "0".ToDecimal());
+                    break;
+                case "datetime":
+                    obj.GetType().GetProperty(property.Name).SetValue(obj, DateTime.Now);
+                    break;
+                default:
+                    break;
+            };
+        }
+        return obj;
+    }//ToDefaultValue
+
+    public static T ToValue<T>(this T obj, object req) {
+        foreach (var propertyReq in req.GetType().GetProperties()) {
+            if (req.GetType().GetProperty(propertyReq.Name).GetValue(req) == null) continue;
+            var pName = propertyReq.Name;
+            var pValue = req.GetType().GetProperty(propertyReq.Name).GetValue(req);
+            foreach (var property in obj.GetType().GetProperties().Where(x => x.Name == pName)) {
+                if (obj.GetType().GetProperty(property.Name).GetValue(obj) != null) continue;
+                switch (property.PropertyType.Name.ToLower()) {
+                    case "string":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "int":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "int32":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "int64":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "double":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "decimal":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    case "datetime":
+                        obj.GetType().GetProperty(property.Name).SetValue(obj, pValue);
+                        break;
+                    default:
+                        break;
+                };
+            }
+        }
+        return obj;
+    }//ToValue
+
+}//class
---
+++ library/StartFMS.Extensions.Data/ObjectConvert.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFMS.Extensions.Data {
+    internal static class ObjectConvert {
+        public static Decimal ToDecimal(this String str) {
+            return decimal.TryParse(str, out decimal result) ? result : 0;
+        }
+        public static DateTime ToDateTime(this String str) {
+            return DateTime.TryParse(str, out DateTime result) ? result : new DateTime();
+        }
+
+        public static string ToDateTimeFormat(this String str, string format = "yyyy-MM-dd HH:mm:ss") {
+            return DateTime.TryParse(str, out DateTime result) ? result.ToString(format) : "";
+        }
+
+        public static Decimal ToDecimal(this Object str) {
+            return str != null && decimal.TryParse(str.ToString(), out decimal result) ? result : 0;
+        }
+        public static Boolean ToBoolean(this Object str) {
+            return Boolean.TryParse(str.ToString(), out Boolean result) && result;
+        }
+
+        public static int ToInt(this Object str) {
+            return int.TryParse(str.ToString(), out int result) ? result : 0;
+        }
+
+        public static Int32 ToInt32(this Object str) {
+            return Int32.TryParse(str.ToString(), out Int32 result) ? result : 0;
+        }
+
+        public static Int64 ToInt64(this Object str) {
+            return Int64.TryParse(str.ToString(), out Int64 result) ? result : 0;
+        }
+        public static Double ToDouble(this Object str) {
+            return Double.TryParse(str.ToString(), out Double result) ? result : 0;
+        }
+        public static string ToDecimalPlaces(this string str, int digits = 4) {
+            if (!str.Contains('.')) { str += ".0000"; }
+            if (!decimal.TryParse(str, out decimal oDecimal)) { return str; }
+            decimal before = decimal.Parse(str);
+            return decimal.Round(before, digits).ToString();
+        }
+
+        public static string ToThousandths(this string str) {
+            if (!decimal.TryParse(str, out decimal oDecimal)) { return str; }
+            decimal before = decimal.Parse(str);
+            return String.Format("{0:N}", before);
+        }
+
+        public static string ToStringNumber(this int number, int digits = 4) {
+            string nowNumber = (number).ToString();
+            string template = "";
+            int len = nowNumber.Length;
+            if (len >= digits) { return nowNumber; }
+            for (int index = 1; index <= (digits - len); index++) {
+                template += "0";
+            }
+            return template + nowNumber;
+        }
+        public static string ToStringAutoNumber(this int number, int digits = 4) {
+            string nowNumber = (number + 1).ToString();
+            string template = "";
+            int len = nowNumber.Length;
+            if (len >= digits) { return nowNumber; }
+            for (int index = 1; index <= (digits - len); index++) {
+                template += "0";
+            }
+            return template + nowNumber;
+        }
+    }
+}
