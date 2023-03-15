f40d5ab3b833b1b24b10cebb2ad2dd1ea23414cc
jontcont <52921053+JontCont@users.noreply.github.com>
2023-03-16 02:43:03 +0800

    加入 Configuration  類別庫

---
+++ library/StartFMS.Extensions.Configuration/Config.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Formats.Asn1;
+
+namespace StartFMS.Extensions.Configuration;
+public static class Config 
+{
+    /// <summary>
+    /// 取得 ConnectionStrings 底下資料
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string GetConnectionString(string name) {
+        IConfiguration config = GetConfiguration();
+        return config.GetConnectionString(name);
+    }
+
+    public static IConfiguration GetAzureConfiguration(string connectionString) {
+        if(string.IsNullOrEmpty(connectionString)) {
+            throw new ArgumentNullException($"Error : {nameof(connectionString)} 不可為Null");
+        }
+
+        var builder = new ConfigurationBuilder();
+        if (connectionString != null) {
+            // 加入 Azure App Configuration 資料源
+            builder.AddAzureAppConfiguration(options => {
+                options.Connect(connectionString)
+                    // 如果您想要只加載指定的鍵，可以使用 Select 方法，例如：
+                    //.Select(KeyFilter.AnyOf("MyApp:*"))
+                    .ConfigureKeyVault(kv => {
+                        kv.SetCredential(new DefaultAzureCredential());
+                    });
+            });
+        }
+        return builder.Build();
+    }
+
+    /// <summary>
+    /// Get Local Environment / appsetting.json 
+    /// </summary>
+    /// <returns></returns>
+    public static IConfiguration GetConfiguration() {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        return builder.Build();
+    }
+
+    /// <summary>
+    /// Get Local Environment / appsetting.json / UserSecrets
+    /// </summary>
+    /// <returns></returns>
+    public static IConfiguration GetAzureConfiguration<T>() {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets(typeof(T).Assembly, optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables();
+        return builder.Build();
+    }
+}
