c47cbb4ea79338c2d11f0f0ebc8842eb9a5a8008
jontcont <52921053+JontCont@users.noreply.github.com>
2023-04-04 19:24:53 +0800

    加入Line 單元測試

--- UnitTest/StartFMS.Extensions.LineTests/LineBotsTests.cs
+++ UnitTest/StartFMS.Extensions.LineTests/LineBotsTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using isRock.LineBot;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StartFMS.Extensions.Line;
 using System;
 using System.Collections.Generic;
@@ -11,16 +13,12 @@ namespace StartFMS.UnitTest.Line
     [TestClass()]
     public class LineBotsTests
     {
-        [TestMethod()]
-        public void ExecuteReaderTest()
-        {
-            throw new NotImplementedException();
-        }
 
         [TestMethod()]
         public void MessageTest()
         {
-            throw new NotImplementedException();
+            new LineBots().Message("image");
         }
+
     }
 }
