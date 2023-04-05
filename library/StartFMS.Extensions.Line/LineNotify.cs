8764a5af77a7a62287845c869214523dd9f2a6b5
jontcont <52921053+JontCont@users.noreply.github.com>
2023-04-05 19:21:45 +0800

    加入LineNotify 功能

---
+++ library/StartFMS.Extensions.Line/LineNotify.cs
@@ -0,0 +1,20 @@
+﻿using isRock.LineNotify;
+
+namespace StartFMS.Extensions.Line
+{
+    public class LineNotify : IDisposable
+    {
+        public string ChannelToken { get; set; }
+
+        public void Send(string text)
+        {
+            Utility.SendNotify(ChannelToken, text);
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
