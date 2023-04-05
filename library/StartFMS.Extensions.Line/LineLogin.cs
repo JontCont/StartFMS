ca55c699c2f4f2c7928365faf4e9a2e2561b20a6
jontcont <52921053+JontCont@users.noreply.github.com>
2023-04-05 19:22:02 +0800

    加入 LineLogin

---
+++ library/StartFMS.Extensions.Line/LineLogin.cs
@@ -0,0 +1,62 @@
+﻿using isRock.LineLoginV1;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFMS.Extensions.Line
+{
+    public class LineLogin : IDisposable
+    {
+        public string ChannelToken { get; set; }
+        public string AdminUserID { get; set; }
+        public UrlRequest urlRequest { get; set; }
+
+        public class UrlRequest
+        {
+            public string url { set; get; }
+            public string response_type { get; set; }
+            public string client_id { get; set; }
+            public string redirect_uri { get; set; }
+            public string scope { get; set; }
+            public string state { get; set; }
+        }
+
+        public string GetLoginUrl()
+        {
+            string resultUrl = urlRequest.url;
+            List<string> resultData = new();
+            var props = typeof(UrlRequest).GetProperties();
+            foreach (var prop in props)
+            {
+                string paramName = prop.Name;
+                var paramValue = prop.GetValue(urlRequest);
+                if (paramValue == null && paramName == "url") { continue; }
+                resultData.Add(paramName + "=" + paramValue.ToString());
+            }
+            return resultUrl + "?" + string.Join("&", resultData);
+        }
+
+        public isRock.LineLoginV21.GetTokenFromCodeResult? GetTokenFromCode(string code)
+        {
+            var token = isRock.LineLoginV21.Utility.GetTokenFromCode(code,
+              urlRequest.client_id,
+              ChannelToken,
+              urlRequest.redirect_uri);
+            return token;
+        }
+
+        public isRock.LineLoginV21.Profile? GetUserProfile(string accessToken)
+        {
+            var profile = isRock.LineLoginV21.Utility.GetUserProfile(accessToken);
+            return profile;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
