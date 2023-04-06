1a1fe0f8647a3db14abbb43f30fcbaec6185434a
jontcont <52921053+JontCont@users.noreply.github.com>
2023-04-06 11:44:58 +0800

    加入 Line Notify 驗證

--- library/StartFMS.Extensions.Line/LineNotify.cs
+++ library/StartFMS.Extensions.Line/LineNotify.cs
@@ -5,12 +5,73 @@ namespace StartFMS.Extensions.Line
     public class LineNotify : IDisposable
     {
         public string ChannelToken { get; set; }
+        public string DeveloperToken { get; set; }
+        public UrlRequest urlRequest { get; set; }
 
-        public void Send(string text)
+        public class UrlRequest
         {
-            Utility.SendNotify(ChannelToken, text);
+            public string url { set; get; }
+            public string response_type { get; set; }
+            public string client_id { get; set; }
+            public string redirect_uri { get; set; }
+            public string scope { get; set; }
+            public string state { get; set; }
         }
 
+        public string GetNotifyUrl()
+        {
+            string resultUrl = urlRequest.url;
+            List<string> resultData = new();
+            var props = typeof(UrlRequest).GetProperties();
+            foreach (var prop in props)
+            {
+                string paramName = prop.Name;
+                var paramValue = prop.GetValue(urlRequest);
+                if (paramValue == null || paramName == "url") { continue; }
+                resultData.Add(paramName + "=" + paramValue.ToString());
+            }
+            return resultUrl + "?" + string.Join("&", resultData);
+        }
+
+        public void DeveloperSend(string text)
+        {
+            Utility.SendNotify(DeveloperToken, text);
+        }
+
+        public void DeveloperSend(string text, Uri imageThumbnail, Uri imageFullsize, bool notificationDisabled = false)
+        {
+            Utility.SendNotify(DeveloperToken, text, imageThumbnail, imageFullsize, 0 , 0 ,notificationDisabled);
+        }
+
+        public void DeveloperSend(string text, int stickerPackageId, int stickerId, bool notificationDisabled = false)
+        {
+            Utility.SendNotify(DeveloperToken, text,null,null, stickerPackageId, stickerId, notificationDisabled);
+        }
+
+
+        public isRock.LineNotify.GetTokenFromCodeResult? GetTokenFromCode(string code)
+        {
+            var token = isRock.LineNotify.Utility.GetTokenFromCode(code,
+              urlRequest.client_id,
+              ChannelToken,
+              urlRequest.redirect_uri);
+            return token;
+        }
+
+        public void Send(string text,string accessToken)
+        {
+            Utility.SendNotify(accessToken, text);
+        }
+
+        public void Send(string text, string accessToken, Uri imageThumbnail, Uri imageFullsize, bool notificationDisabled = false)
+        {
+            Utility.SendNotify(accessToken, text, imageThumbnail, imageFullsize, 0, 0, notificationDisabled);
+        }
+
+        public void Send(string text, string accessToken, int stickerPackageId, int stickerId, bool notificationDisabled = false)
+        {
+            Utility.SendNotify(accessToken, text, null, null, stickerPackageId, stickerId, notificationDisabled);
+        }
 
         public void Dispose()
         {
